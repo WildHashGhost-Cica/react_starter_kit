c59248b33fb52e410eb9cd5936e1e55da1e150cd
WildHashGhost_Cica <wildhashghost@gmail.com>
2021-07-06 18:34:55 +0200

    css and styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-
-
-
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Feature from './components/Feature';
+import Footer from './components/Footer';
+import Hero from './components/Hero';
+import Products from './components/Products';
+import { productData, productDataTwo } from './components/Products/data';
+import { GlobalStyle } from './globalStyles';
 function App() {
   return (
-    <div className="App">
-      WildHashGhost
-    </div>
+    <Router>
+      <GlobalStyle/>
+      <Hero/>
+      <Products heading='Choose your favorite' data={productData}/>
+      <Feature/>
+      <Products heading='Other Creation from me' data={productDataTwo}/>
+      <Footer/>
+    </Router>
   );
 }
 
